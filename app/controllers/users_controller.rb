5ad76712147b298de1b987e9c9b33495bb3f4683
nextacademy <yithanglee@gmail.com>
2016-11-22 21:04:54 +0800

    broke

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,37 @@
 class UsersController < ApplicationController
 
-def edit
+before_action :set_user, only:[:show, :edit, :update]
+
+def show
 	
+end
+
+def edit
+
 end
 
 
 def update
+ 
+      if @user.update(user_params)
+        redirect_to root_url, notice: 'User was successfully updated.'
+      else
+      	redirect_to root_url, alert: 'User was not successfully updated.'      
+      end
+
+end
+
+private
+
+def set_user
+
+	@user = User.find(params[:id])
+	
+end
+
+def user_params
+
+	params.require(:user).permit(:name, :email, :password)
 	
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,26 @@
 Rails.application.routes.draw do
 
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+  get '/users/:id', to: 'users#show', as: 'user'
+
+  get "/users/:id/edit", to: 'users#edit', as: 'user_edit'
+  patch "/users/:id", to: 'users#update', as: 'user_update'
+
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
   get '/search', to: 'books#search'
 
   post '/books/quick_create', to: 'books#quick_create', as: 'quick_create_book'
