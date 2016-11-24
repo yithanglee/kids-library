0295ee928c753e04eb769c25f1622f3327c35021
nextacademy <yithanglee@gmail.com>
2016-11-24 11:33:44 +0800

    config file change to make searchkickwork

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,8 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  Searchkick.client = Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
