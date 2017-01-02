3fb837ac9f109eb50c2b8002f58796a8d444c48f
Damien Lee <yithanglee@gmail.com>
2017-01-03 07:05:45 +0800

    init

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -9,48 +9,57 @@ class StaticpagesController < ApplicationController
   end
 
   def help
+
+  end
+
+
+def import_books
+    @paragraph = {}
+    @lines = []
+    # need to do a migration using this page, will open a file from the root, then tries to import it, never tried a chinese file before, but will attempt to.
+    File.open("2016_07_newbooks_damien.txt", "r:UTF-16").each do |line|
+      @lines << line
+    end
+    n = @lines.count
+
+    for i in (1..n-1)
+      row = @lines[i].chop.split("\t")
+      @book = Book.new(
+        name: row[0],
+        isbn: row[9],
+        barcode: row[10],
+        author_1: row[1],
+        author_2: row[2],
+        publisher: row[4],
+        series: row[5],
+        volume: row[6],
+        price: row[11]
+        )
+      @book.save
+      @categories = []
+      @categories << row[7].split.first
+      @categories << row[8]
+
+      @categories.each do |x|
+          a = Category.find_by(name: x)
+          a.books << @book
+        end
+    
+    end
+    return @lines
+end
+
+def print_book_barcodes
     require 'barby/barcode/ean_13'
     require 'barby/outputter/html_outputter'
     # outputter = Barby::HtmlOutputter.new(barcode)
     # outputter.to_html
     # barcode.to_html
 
-  	# @paragraph = {}
-  	# @lines = []
-  	# # need to do a migration using this page, will open a file from the root, then tries to import it, never tried a chinese file before, but will attempt to.
-  	# File.open("2016_07_newbooks_damien.txt", "r:UTF-16").each do |line|
-  	#  	@lines << line
-  	# end
-  	# n = @lines.count
+end
 
-  	# for i in (1..n-1)
-  	# 	row = @lines[i].chop.split("\t")
-  	# 	@book = Book.new(
-  	# 		name: row[0],
-  	# 		isbn: row[9],
-  	# 		barcode: row[10],
-  	# 		author_1: row[1],
-  	# 		author_2: row[2],
-  	# 		publisher: row[4],
-  	# 		series: row[5],
-  	# 		volume: row[6],
-  	# 		price: row[11]
-  	# 		)
-  	# 	@book.save
-  	# 	@categories = []
-  	# 	@categories << row[7].split.first
-  	# 	@categories << row[8]
-
-  	# 	@categories.each do |x|
-   #        a = Category.find_by(name: x)
-   #        a.books << @book
-   #      end
-  	
-  	# end
-  	# return @lines
-
-
-  end
-
+def print_member_cards
+  
+end
 
 end
